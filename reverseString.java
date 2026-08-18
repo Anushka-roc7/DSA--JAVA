bb78760ab79a89683b1a8d81fd2031c8608a9fec
Anushka-roc7 <anushkarpm007@gmail.com>
2026-08-19 00:33:40 +0530

    Add files via upload

---
+++ reverseString.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class reverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int freq[] = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            freq[str.charAt(i) - 'a']++;
+
+        }
+        boolean found = false;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (freq[ch - 'a'] == 1) {
+                System.out.println(ch);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("no unique character");
+        }
+    }
+
+}
