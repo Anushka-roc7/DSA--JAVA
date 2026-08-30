ba03150038644625417a623ebdd8ff821d90f6fc
Anushka-roc7 <anushkarpm007@gmail.com>
2026-08-30 21:15:18 +0530

    Add files via upload

---
+++ checktwostringareanagram.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class checktwostringareanagram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        char[] a = s1.toCharArray();
+        char[] b = s2.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        if (Arrays.equals(a, b)) {
+            System.out.println("Anagram");
+        } else {
+            System.out.println("not anagram");
+        }
+    }
+
+}
